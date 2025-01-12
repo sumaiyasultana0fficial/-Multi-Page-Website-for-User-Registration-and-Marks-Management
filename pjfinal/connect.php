d040044333d8df3b3ca98d4c7d714223e345745b
Sumaiya Sultana <96431879+sumaiyasultana0fficial@users.noreply.github.com>
2025-01-13 01:53:55 +0600

    Add files via upload

---
+++ pjfinal/connect.php
@@ -0,0 +1,35 @@
+<?php
+   $servername="localhost";
+   $username="root";
+   $password="";
+   $dbname="project";
+    
+
+   $con = mysqli_connect($servername,$username,$password,$dbname);
+ 
+   if (isset($_POST['Submit'])) {
+    $name = $_POST['fullname'];
+    $id = $_POST['id'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $dept = $_POST['dept'];
+    $dob = $_POST['dob'];
+    $religion = $_POST['religion'];
+    $gender = $_POST['gender'];
+    $term = $_POST['term'];
+    $phn = $_POST['phn'];
+    $emphn = $_POST['emphn'];
+	$chk_religion= implode(",",$religion);
+	
+	
+
+if($name!="" &&$id!="" && $email!="" &&$password!="" && $dept!="" && $dob!="" && $religion!="" && $gender!="" && $term!="" && $phn!="" && $emphn!=""){    
+   $query="INSERT INTO REGISTATION VALUES('$name','$id','$email','$password','$dept','$dob','$chk_religion','$gender','$term','$phn','$emphn')";
+   $data=mysqli_query($con,$query);
+}
+   else{
+	echo "not insert";
+   }
+}
+
+?>
